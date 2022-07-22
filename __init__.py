523385c160de2c7a13bd5c3e0e15a659b7737344
DarthLilo <darthdaxx@comcast.net>
2022-07-21 18:04:45 -0700

    1.4.2 Post Release Patch

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info ={
     "name": "Ice Cube",
     "author": "DarhtLilo",
-    "version": (1, 4, 1),
+    "version": (1, 4, 2),
     "blender": (3, 0, 0),
     "location": "View3D > Tool",
     "description": "A custom python script for Ice Cube! Credit to \"@KJMineImator\" and \"@RealMineAPI\" on twitter for helping me with the code!",
@@ -16,7 +16,7 @@ import os
 
 #File Variables
 root_folder = os.path.dirname(os.path.abspath(__file__))
-github_url = "https://api.github.com/repos/DarthLilo/Ice-Cube/releases/latest"
+github_url = "https://api.github.com/repos/DarthLilo/ice_cube/releases/latest"
 latest_dlc = "https://raw.githubusercontent.com/DarthLilo/ice_cube/master/ice_cube_data/dlc_list.json"
 dlc_id = []
 dlc_type = []
@@ -26,6 +26,17 @@ dlc_enum_data = []
 update_available = False
 
 #Folder Creation
+required_dirsmain = ["backups","downloads"]
+
+for dir in required_dirsmain:
+    dir_path = os.path.normpath(f"{root_folder}/{dir}")
+    if os.path.exists(dir_path):
+        print(f"Found path {dir_path}")
+    else:
+        os.mkdir(dir_path)
+        print(f"Made directory {dir_path}")
+
+
 required_dirs = ["skins","user_packs", "user_packs/rigs", "user_packs/inventory"]
 for dir in required_dirs:
     dir_path = os.path.normpath(f"{root_folder}/ice_cube_data/internal_files/{dir}")
