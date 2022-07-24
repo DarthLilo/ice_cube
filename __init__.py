7f33edfa15da83d48e3db24f61947db65b332cdf
BlueEvilGFX <107540992+BlueEvilGFX@users.noreply.github.com>
2022-07-24 13:35:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,9 @@ import importlib
 import sys
 import os
 
+#Print Informations
+print_information = False
+
 #File Variables
 root_folder = os.path.dirname(os.path.abspath(__file__))
 github_url = "https://api.github.com/repos/DarthLilo/ice_cube/releases/latest"
@@ -30,21 +33,23 @@ required_dirsmain = ["backups","downloads"]
 
 for dir in required_dirsmain:
     dir_path = os.path.normpath(f"{root_folder}/{dir}")
-    if os.path.exists(dir_path):
+    if os.path.exists(dir_path) and print_information:
         print(f"Found path {dir_path}")
     else:
         os.mkdir(dir_path)
-        print(f"Made directory {dir_path}")
+        if print_information:
+            print(f"Made directory {dir_path}")
 
 
 required_dirs = ["skins","user_packs", "user_packs/rigs", "user_packs/inventory"]
 for dir in required_dirs:
     dir_path = os.path.normpath(f"{root_folder}/ice_cube_data/internal_files/{dir}")
-    if os.path.exists(dir_path):
+    if os.path.exists(dir_path) and print_information:
         print(f"Found path {dir_path}")
     else:
         os.mkdir(dir_path)
-        print(f"Made directory {dir_path}")
+        if print_information:
+            print(f"Made directory {dir_path}")
 
 
 #Path Appending
